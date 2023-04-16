d9145e2e0e8089b1ecd07351a9b59ac80e2d6ac3
gopii8 <gopithammisetti6@gmail.com>
2023-04-16 05:34:19 +0530

    Prime Number Checking Using Normal

---
+++ PrimeNumberCheck.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class PrimeNumberCheck {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter a Number to check it is prime or not");
+        int n=s.nextInt();
+        int flag=0;
+        if(n==0 || n==1 ){
+            flag=1;
+
+        }
+        for(int i=2;i<n/2;i++){
+            if(n%i==0){
+                flag=1;
+            }
+        }
+        if(flag==1){
+            System.out.println("Not a Prime");
+        }else System.out.println("Prime");
+    }
+}
