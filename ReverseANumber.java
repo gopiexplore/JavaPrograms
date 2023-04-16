48a707dddf26d4f0f34ab5c02090c07368a32bc6
gopii8 <gopithammisetti6@gmail.com>
2023-04-16 11:35:12 +0530

    Reverse of a Number Using Method

--- ReverseANumber.java
+++ ReverseANumber.java
@@ -1,16 +1,20 @@
 import java.util.Scanner;
 
 public class ReverseANumber {
-    public static void main(String[] args) {
-        Scanner s=new Scanner(System.in);
-        System.out.println("Enter a Number to Find Reverse of a Number");
-        int a=s.nextInt();
+    public  static  void reverseNumber(int a){
         int rev=0;
         while (a>0){
             rev=(rev*10)+a%10;
             a=a/10;
         }
         System.out.println("Reverse of Number is "+rev);
+    }
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter a Number to Find Reverse of a Number");
+        int a=s.nextInt();
+        reverseNumber(a);
+
 
     }
 }
