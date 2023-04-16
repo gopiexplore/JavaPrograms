89d0a1bebe3b23e1e5fed08c42cc10891b0e0166
gopii8 <gopithammisetti6@gmail.com>
2023-04-16 05:41:48 +0530

    Checked the Prime Number or NOt Using Method

--- PrimeNumberCheck.java
+++ PrimeNumberCheck.java
@@ -1,22 +1,25 @@
 import java.util.Scanner;
 
 public class PrimeNumberCheck {
-    public static void main(String[] args) {
-        Scanner s=new Scanner(System.in);
-        System.out.println("Enter a Number to check it is prime or not");
-        int n=s.nextInt();
+    static  void primeNumber(int n){
         int flag=0;
-        if(n==0 || n==1 ){
+        if(n==0 || n==1){
             flag=1;
-
         }
-        for(int i=2;i<n/2;i++){
+        for (int i=2;i<=Math.sqrt(n);i++){
             if(n%i==0){
                 flag=1;
             }
         }
         if(flag==1){
             System.out.println("Not a Prime");
-        }else System.out.println("Prime");
+        }
+        else System.out.println("Prime");
+    }
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter a Number to check it is prime or not");
+        int n=s.nextInt();
+        primeNumber(n);
     }
 }
